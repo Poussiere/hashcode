d0c3266ebda8a5f759e41e6e581d5cede45cbf86
nboutin <nicolas.boutin@soprasteria>
2019-02-28 18:08:14 +0100

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,13 @@
+import java.io.File
+
+fun main (args : Array<String>){
+/*  val inputFile = File("input")
+    val tabText = inputFile.readLines(Charsets.UTF_8)
+    val firstLine = tabText[0].split(" ").map{it.toInt()}
+    val numberOfVideos = firstLine[0]
+
+    //Output file :
+    val outputFile = File("output.txt")
+    outputFile.appendText("some text")
+    */
+}
